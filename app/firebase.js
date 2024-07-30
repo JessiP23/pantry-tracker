f2090600efae7bfbd8462ea62fea583e4e6fd359
JessiP23 <jessi316866@gmail.com>
2024-07-29 22:29:40 -0400

    pantry initial project

---
+++ app/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+import firebase from "firebase/compat/app";
+// Required for side-effects
+import "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAYWz6JmbSz7399nml8d7QaORhOMaY0nb0",
+  authDomain: "pantry-36337.firebaseapp.com",
+  projectId: "pantry-36337",
+  storageBucket: "pantry-36337.appspot.com",
+  messagingSenderId: "702745019000",
+  appId: "1:702745019000:web:1a302f88fa52e1ea49e38d",
+  measurementId: "G-63VTG6HVEE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+export const db = getFirestore(app);
